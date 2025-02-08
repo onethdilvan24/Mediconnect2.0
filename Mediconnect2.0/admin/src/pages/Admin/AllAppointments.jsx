ac68a7fb254479b88c243db92d50a9ceec3c9462
Ramalsasanka <sasankaiddawela2000@gmail.com>
2025-02-08 23:25:46 +0530

    changes

--- Mediconnect2.0/admin/src/pages/Admin/AllAppointments.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AllAppointments = () => {
-  return (
-    <div>AllAppointments</div>
-  )
-}
-
-export default AllAppointments
--- Mediconnect2.0/admin/src/pages/Admin/DoctorsList.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DoctorsList = () => {
-  return (
-    <div>DoctorsList</div>
-  )
-}
-
-export default DoctorsList
